c5e34d38e751ca7a953e1e79aef5d8dea846f793
Paweł Rokosz <rokosz.pawel@gmail.com>
2017-09-21 13:54:28 +0200

    gulp and files architecture patch

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,60 @@
- const gulp = require('gulp');
- const sass = require('gulp-sass');
- const autoprefixer = require('gulp-autoprefixer');
- const browserSync = require('browser-sync').create();
- const reload = browserSync.reload;
- const imagemin = require('gulp-imagemin');
- const cleanCSS = require('gulp-clean-css');
- const changed = require('gulp-changed');
- const sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const imagemin = require('gulp-imagemin');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
 
- gulp.task('browser-sync', function () {
-    browserSync.init({
-      server: './'
-    });
- });
 
- gulp.task('styles', function () {
-   return gulp.src('./assets/prod/styles/**/*.scss')
-     .pipe(sourcemaps.init())
-       .pipe(sass().on('error', sass.logError))
-     .pipe(sourcemaps.write())
-     .pipe(autoprefixer({
-         browsers: ['last 5 versions'],
-         cascade: false
-     }))
-     .pipe(gulp.dest('./assets/prod/css/'))
-     .pipe(browserSync.stream());
- });
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: './'
+  });
+});
 
- gulp.task('minify-css', () => {
-   return gulp.src('./assets/prod/css/main.css')
-     .pipe(cleanCSS({compatibility: 'ie8'}))
-     .pipe(gulp.dest('./assets/dist/css/'));
- });
+gulp.task('styles', function () {
+ return gulp.src('./src/assets/sass/**/*.scss')
+   .pipe(sourcemaps.init())
+      .pipe(sass().on('error', sass.logError))
+   .pipe(sourcemaps.write())
+   .pipe(autoprefixer({
+       browsers: ['last 5 versions'],
+       cascade: false
+   }))
+   .pipe(gulp.dest('./src/assets/css/'))
+});
 
- gulp.task('image', () =>
- 	gulp.src('./assets/prod/images/*')
-    .pipe(changed('./assets/prod/styles'))
- 		.pipe(imagemin())
- 		.pipe(gulp.dest('./assets/dist/images/'))
- );
+gulp.task('minify-css', () => {
+ return gulp.src('./src/assets/css/main.css')
+   .pipe(cleanCSS({compatibility: 'ie8'}))
+   .pipe(gulp.dest('./dist/css/'))
+   .pipe(browserSync.stream());
+});
 
- gulp.task('watch', function(){
-   gulp.watch('./assets/prod/js/*.js');
-   gulp.watch('./assets/prod/css/main.css', ['minify-css']);
-   gulp.watch('./assets/prod/styles/**/*.scss', ['styles']);
-   gulp.watch('*.html').on('change', reload);
- });
+gulp.task('image', () =>
+	gulp.src('src/assets/images/**.*')
+		.pipe(imagemin({progressive: true}))
+		.pipe(gulp.dest('dist/images'))
+);
 
- gulp.task('default', ['image', 'minify-css', 'styles', 'browser-sync', 'watch']);
+gulp.task("scripts", function() {
+  return gulp.src(['src/assets/js/*.js'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({presets: ['es2015']}))
+    .pipe(concat("all.min.js"))
+    .pipe(uglify().on('error', function(e){
+       console.log(e);
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./dist/js"))
+});
+
+gulp.task('watch', function(){
+ gulp.watch('./src/assets/sass/**/*.scss', ['styles']);
+ gulp.watch('./src/assets/css/main.css', ['minify-css']);
+ gulp.watch('./**/*.html').on('change', reload);
+});
+
+gulp.task('default', ['image', 'styles', 'minify-css', 'browser-sync', 'watch']);
